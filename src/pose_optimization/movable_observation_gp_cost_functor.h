bd711d05483f80cd1177da9edd29192da9182009
Amanda Adkins <mandi1267@gmail.com>
2020-12-03 18:20:52 -0600

    Convert the cost functor to take a 3d robot pose and then project the observed object into 2d

--- src/pose_optimization/movable_observation_gp_cost_functor.h
+++ src/pose_optimization/movable_observation_gp_cost_functor.h
@@ -29,35 +29,39 @@ namespace pose_optimization {
             float obs_x = observation(0, 0);
             float obs_y = observation(1, 0);
             float observation_angle = observation(2, 0);
-            observation_transform_ << cos(observation_angle), (-sin(observation_angle)), obs_x,
-                    sin(observation_angle), cos(observation_angle), obs_y,
-                    0, 0, 1;
+            observation_transform_.translation() = Eigen::Vector3f(obs_x, obs_y, 0.0);
+            Eigen::Matrix3f rotation_mat = Eigen::Matrix3f::Identity();
+            rotation_mat.block<2, 2>(0, 0) = Eigen::Rotation2Df(observation_angle).toRotationMatrix();
+            observation_transform_.linear() = rotation_mat;
         }
 
         template<typename T>
-        bool operator()(T *robot_pose_ptr, T *residuals) {
+        bool operator()(T *robot_position_ptr, T* robot_orientation_ptr, T *residuals) {
 
-            // Assuming robot pose is a 3-entry vector with x, y, theta.
-            Eigen::Matrix<T, 3, 3> robot_transform;
-            T robot_angle = robot_pose_ptr[2];
-            robot_transform << cos(robot_angle), (-sin(robot_angle)), robot_pose_ptr[0],
-                    sin(robot_angle), cos(robot_angle), robot_pose_ptr[1],
-                    0, 0, 1;
+            // Assuming robot_position_ptr is
+            Eigen::Map<const Eigen::Matrix<T, 3, 1>> robot_position(robot_orientation_ptr);
+            Eigen::Map<const Eigen::Quaternion<T>> robot_orientation(robot_orientation_ptr);
 
-            Eigen::Matrix<T, 3, 3> transformed_object = robot_transform * observation_transform_;
-            Eigen::Matrix<T, 3, 1> transformed_object_pose;
-            Eigen::Rotation2D <T> rotation(transformed_object.template block<2, 2>());
-            transformed_object_pose << transformed_object(0, 2), transformed_object(1, 2), rotation.angle();
-            gp_->Inference<T>(transformed_object_pose);
+            Eigen::Transform<T,3,Eigen::Affine> robot_tf = Eigen::Transform<T, 3, Eigen::Affine>::Identity();
+            robot_tf.translation() = robot_position;
+            robot_tf.linear() = robot_orientation.toRotationMatrix();
+
+            Eigen::Transform<T, 3, Eigen::Affine> observation_3d = robot_tf * observation_transform_;
+            // TODO verify that yaw extraction is correct
+            T yaw = observation_3d.rotation().eulerAngles(0, 1, 2)[2];
+
+            Eigen::Matrix<T, 3, 1> object_pose_2d;
+            object_pose_2d << observation_transform_.translation().x(), observation_transform_.translation().y(), yaw;
+            gp_->Inference<T>(object_pose_2d);
 
             // TODO is this the correct form for the cost?
-            residuals[0] = -log(gp_->Inference<T>(transformed_object_pose));
+            residuals[0] = -log(gp_->Inference<T>(object_pose_2d));
             return true;
         }
 
         gp_regression::GaussianProcessRegression<3, 1, gp_kernel::Pose2dKernel> *gp_;
         Eigen::Matrix<float, 3, 1> &observation_;
-        Eigen::Matrix<float, 3, 3> observation_transform_;
+        Eigen::Affine3f observation_transform_;
     };
 }
 
