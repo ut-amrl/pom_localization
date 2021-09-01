52d288cdbe2545d9782f5b381db08376ed11f8c9
Amanda Adkins <mandi1267@gmail.com>
2021-08-31 19:00:44 -0500

    Adjust to run on KITTI; might need to change max GPC samples to 500

--- src/evaluation/runtime_params_config_writer.cc
+++ src/evaluation/runtime_params_config_writer.cc
@@ -19,24 +19,24 @@ int main(int argc, char **argv) {
     std::string runtime_params_config_file = FLAGS_cfg_file_name;
     file_io::RuntimeParamsConfig runtime_params_config;
 
-    runtime_params_config.max_gpc_samples_ = 500;
+    runtime_params_config.max_gpc_samples_ = std::numeric_limits<uint64_t>::max(); // TOOD set this
 
-    runtime_params_config.num_nodes_in_optimization_window_ = 30;
-    runtime_params_config.full_optimization_interval_ = 80;
+    runtime_params_config.num_nodes_in_optimization_window_ = 60; // TODO set this
+    runtime_params_config.full_optimization_interval_ = 60; // TODO set this
 
-    runtime_params_config.mean_position_kernel_len_ = 0.6;
-    runtime_params_config.mean_orientation_kernel_len_ = 0.2;
+    runtime_params_config.mean_position_kernel_len_ = 1;
+    runtime_params_config.mean_orientation_kernel_len_ = 0.5;
 
     runtime_params_config.mean_position_kernel_var_ = 0.045;
-    runtime_params_config.mean_orientation_kernel_var_ = 10000;
+    runtime_params_config.mean_orientation_kernel_var_ = 1;
 
     runtime_params_config.default_obj_probability_input_variance_for_mean_ = 10;
 
-    runtime_params_config.var_position_kernel_len_ = 7.5;
+    runtime_params_config.var_position_kernel_len_ = 25;
     runtime_params_config.var_orientation_kernel_len_ = 20;
 
-    runtime_params_config.var_position_kernel_var_ = 30;
-    runtime_params_config.var_orientation_kernel_var_ = 0.3;
+    runtime_params_config.var_position_kernel_var_ = 0.003;
+    runtime_params_config.var_orientation_kernel_var_ = 0.003;
 
     runtime_params_config.default_obj_probability_input_variance_for_var_ = 10;
 
@@ -52,12 +52,12 @@ int main(int argc, char **argv) {
 //    runtime_params_config.odom_k6_ = 2;
 
 
-    runtime_params_config.odom_k1_ = 0.75;
-    runtime_params_config.odom_k2_ = 5;
-    runtime_params_config.odom_k3_ = 0.75;
-    runtime_params_config.odom_k4_ = 5;
-    runtime_params_config.odom_k5_ = 2;
-    runtime_params_config.odom_k6_ = 5;
+    runtime_params_config.odom_k1_ = 0.1;
+    runtime_params_config.odom_k2_ = 0.001;
+    runtime_params_config.odom_k3_ = 0.01;
+    runtime_params_config.odom_k4_ = 0.0001;
+    runtime_params_config.odom_k5_ = 0.001;
+    runtime_params_config.odom_k6_ = 0.01;
 
 //    runtime_params_config.odom_k1_ = 0.1;
 //    runtime_params_config.odom_k2_ = 0.2;
